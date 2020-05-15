b8f1905bfb4f874e83e94590eb33d465f5cc17e2
unknown <graham.flaspoehler@paradyme.us>
2020-05-15 12:07:44 -0400

    Make all() method more RESTful
    - CollectionModel<> is a Spring HATEOAS container that encapsulates collections.
    
    - In REST, we should be encapuslating collections of employee resources
      - fetch all employees, then turn them into a list of EntityModel<Employee> objects

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EmployeeController {
@@ -25,8 +26,17 @@ public class EmployeeController {
   //Aggregate root
 
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> all() {
+
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+      .map(employee -> new EntityModel<>(employee,
+        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+        ))
+      .collect(Collectors.toList());
+
+    return new CollectionModel<>(employees,
+      linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
 
   @PostMapping("/employees")
